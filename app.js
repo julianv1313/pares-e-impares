68c1dd659bbeda776a2762cafd7dae9c0bc3e386
julianv1313 <jv.julian@hotmail.com>
2021-06-01 22:27:43 -0500

    usando for

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 app.set('view engine', 'pug');
 app.set('views', 'views');
 
-app.get('/', (req, res) => {
-    res.render('index');
+app.get("/", (req, res) => {
+  const texts = [];
+  for (let i=1; i <=50; i++) {
+    texts.push(`${i} Soy ${(i % 2 == 0 ? "Par" : "Impar")}!`);
+  }
+
+  res.render("index", { texts: texts });
 });
 
-app.listen(3000, () => console.log('Listening on port 3000!'));
+app.listen(3000);
